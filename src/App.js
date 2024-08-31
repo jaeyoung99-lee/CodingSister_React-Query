63ff2805db70c7fa86223dfb0adf5254d70c6dd6
jaeyoung99-lee <leejy0715@naver.com>
2024-08-31 19:58:26 +0900

    리액트 쿼리 사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./HomePage";
+import ReactQueryPage from "./ReactQueryPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <nav style={{ backgroundColor: "beige", padding: "20px" }}>
+        <Link to="/" style={{ marginRight: "10px" }}>
+          HomePage
+        </Link>
+        <Link to="/react/query">React Query</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/react-query" element={<ReactQueryPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
